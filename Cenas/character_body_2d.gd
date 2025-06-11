3bc1a1bcf0599710fca50875a4865ae083371442
Jefferson korte junior <jeffersomkorte@gmail.com>
2025-06-11 18:48:29 -0300

    Adicionei pulo duplo ao personagem

--- Cenas/character_body_2d.gd
+++ Cenas/character_body_2d.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 @export var inv: Inv
@@ -8,23 +7,25 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+var jumps_left = 2  # número de pulos restantes
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Aplica gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jumps_left = 2  # reseta os pulos ao tocar o chão
 
-	# Handle jump.
-	if Input.is_action_just_pressed("pular") and is_on_floor():
+	# Pulo (inclusive duplo)
+	if Input.is_action_just_pressed("pular") and jumps_left > 0:
 		velocity.y = JUMP_VELOCITY
 		audio_stream_player_2d.playing = true
+		jumps_left -= 1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movimento horizontal
 	var direction := Input.get_axis("esquerda", "direita")
-	
-	
-	# animações
+
+	# Animações
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite_2d.play("parado")
@@ -32,22 +33,18 @@ func _physics_process(delta: float) -> void:
 			animated_sprite_2d.play("corre")
 	else:
 		animated_sprite_2d.play("pula")
-		
-	
-	#virar personagem
+
+	# Espelhamento do sprite
 	if direction > 0:
 		animated_sprite_2d.flip_h = false
 	elif direction < 0:
 		animated_sprite_2d.flip_h = true
-	
-	# direção para onde corre
+
+	# Movimento
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		
-	
 
 	move_and_slide()
 
