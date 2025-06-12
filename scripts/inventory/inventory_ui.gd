071023747453d2841ad345f91ed5d0481d315de0
BrunoGraveto <alcantarasantosbruno@gmail.com>
2025-06-12 15:21:55 -0300

    quantidades dos itens nos slots adicionada e corrigida função remover

--- scripts/inventory/inventory_ui.gd
+++ scripts/inventory/inventory_ui.gd
@@ -5,35 +5,53 @@ var inventory = [null, null, null, null]
 
 # Função que adicionará os itens desejados ao inicio
 func _ready():
-	#var item = ItemDB.getItem(1) # Obtem o item espada pelo banco de dados
-	#var item2 = ItemDB.getItem(2) # Obtem o item espada pelo banco de dados
-	#addItem(item)
-	#addItem(item2)
-	#updateUI()
+	addItem(ItemDB.getItem(1))
 	add_to_group("inventory")
 
 # Adiciona um determinado item no inventário
 func addItem(item):
+	# Verifica se o item já existe no inventário
+	for i in range(len(inventory)):
+		if inventory[i] != null and inventory[i]["id"] == item["id"]:
+			inventory[i]["quantity"] += item["quantity"]
+			updateUI()
+			return true
+
+	# Caso não exista, adiciona no primeiro slot vazio
 	for i in range(len(inventory)):
 		if inventory[i] == null:
 			inventory[i] = item
 			updateUI()
 			return true
+
 	return false
 
-# Remove um determinado item no inventário por index
-func removeItem(slotIndex):
-	if slotIndex >= 0 and slotIndex < len(inventory):
-		inventory[slotIndex] = null
+# Remove determinado item do inventário de acordo com o ID passado
+func removeItem(itemID, quantidade=1): # quantidade -> diminui a qtd antes de realmente remover (até zerar)
+	for i in range(len(inventory)):
+		var item = inventory[i]
+		if item != null and item["id"] == itemID:
+			if item["quantity"] > quantidade:
+				item["quantity"] -= quantidade
+			else:
+				inventory[i] = null
+			updateUI()
+			return
 
 # Atualiza os slots do inventário
 func updateUI():
 	for i in range(len(inventory)):
 		var slotBg = $HBoxContainer.get_node("Slot%d" % i).get_node("SlotBackground")  
 		var slotItem = $HBoxContainer.get_node("Slot%d" % i).get_node("SlotItem")  
+		var slotQtd = $HBoxContainer.get_node("Slot%d" % i).get_node("SlotQtd")  
 		var item = inventory[i]
 		if item != null:
 			slotItem.texture = item["icon"]
 			slotItem.visible = true
+			
+			if item["quantity"] > 1:
+				slotQtd.text = str(item["quantity"])
+			slotQtd.visible = true
 		else:
 			slotItem.visible = false
+			slotQtd.visible = false
