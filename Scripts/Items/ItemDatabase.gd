23807e7b8e72a392ceda8d19d2586937f5d0ec21
GUSTAVOHOOO <gustavomazur7475@gmail.com>
2025-06-19 20:30:21 -0300

    Atualiza arquivos na branch brasil

---
+++ Scripts/Items/ItemDatabase.gd
@@ -0,0 +1,49 @@
+extends Node
+
+# Itens do Jogo
+var items = {
+	# Chave
+	1: {
+		"id": 1,
+		"name": "Chave",
+		"icon": preload("res://Assets/Items/chave_moldura.png"),
+		"scene": preload("res://Cenas/items/chave.tscn"),
+		"quantity": 1
+	},
+	# Refrigerante
+	2: {
+		"id": 2,
+		"name": "Refri Pulante",
+		"icon": preload("res://Assets/Items/refrigerante_moldura.png"),
+		"scene": preload("res://Cenas/Items/refrigerante.tscn"),
+		"quantity": 1
+	},
+	# Tenis
+	3: {
+		"id": 3,
+		"name": "Tênis Veloz",
+		"icon": preload("res://Assets/Items/tenis_moldura.png"),
+		"scene": preload("res://Cenas/Items/tenis.tscn"),
+		"quantity": 1
+	},
+	# Escudo
+	4: {
+		"id": 4,
+		"name": "Escudo",
+		"icon": preload("res://Assets/Items/escudo_moldura.png"),
+		"scene": preload("res://Cenas/Items/escudo.tscn"),
+		"quantity": 1
+	},
+	# Escudo com Espinhos
+	5: {
+		"id": 5,
+		"name": "Escudo com Espinhos",
+		"icon": preload("res://Assets/Items/escudo_espinhos_moldura.png"),
+		"scene": preload("res://Cenas/Items/escudo_com_espinhos.tscn"),
+		"quantity": 1
+	}
+}
+
+# Retorna o item
+func getItem(id):
+	return items.get(id, null)
---
+++ Scripts/fases/fase_4.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+@onready var hud = $CanvasLayer/Hud
+
+var levelName = "fase_4"
+var completed = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# Função para finalizar fase
+
+
+func _on_portas_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("player"):
+		return
+	
+	if completed:
+		return
+	
+	# Verifica chave
+	var inventory_node = get_tree().get_first_node_in_group("inventory")
+	if inventory_node == null:
+		print("Inventário não encontrado.")
+		return
+	
+	var chave = ItemDB.getItem(1)
+	var qtd_chaves = inventory_node.getQtdItem(chave)
+	
+	if qtd_chaves < 1:
+		print("Você precisa da chave!")
+		return
+	
+	# Verifica alavanca
+	var alavanca_node = get_tree().get_first_node_in_group("alavanca")
+	if alavanca_node == null or not alavanca_node.ativada:
+		print("Você precisa ativar a alavanca!")
+		return
+	
+	# Marca como concluída
+	completed = true
+	
+	var tempoDecorrido = hud.get_tempo_decorrido()
+	hud.visible = false
+	SaveManager.update_level_progress(levelName, tempoDecorrido)
+	
+	var estrelas = 1
+	if tempoDecorrido <= SaveManager.recompensaLevels[levelName]["3"]:
+		estrelas = 3
+	elif tempoDecorrido <= SaveManager.recompensaLevels[levelName]["2"]:
+		estrelas = 2
+	
+	hud.mostra_resultado(tempoDecorrido, estrelas)
